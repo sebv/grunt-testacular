d0a873151f02758bff68b3093342e9436c64c982
Sebastien Vincent <seb.vincent@gmail.com>
2012-12-05 16:20:07 +0800

    added nofail options

--- tasks/testacularRun.coffee
+++ tasks/testacularRun.coffee
@@ -15,10 +15,11 @@ module.exports = (grunt) ->
     @data.options.success ?= []
     @data.options.failure ?= []
     @data.options.always ?= []
-
+    @data.options.nofail ?= false
+    
     # run the tests
     runner.run @data, (exitCode) =>
-      success = (exitCode is 0)
+      success = (exitCode is 0) || @data.options.nofail
       # execute optional callback tasks
       grunt.task.run if success then @data.options.success else @data.options.failure
       grunt.task.run @data.options.always
--- tasks/testacularRun.js
+++ tasks/testacularRun.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   var runner;
   runner = require('testacular').runner;
   return grunt.registerMultiTask('testacularRun', 'Run tests on a testacular server. ', function() {
-    var done, _base, _base1, _base2, _ref, _ref1, _ref2,
+    var done, _base, _base1, _base2, _base3, _ref, _ref1, _ref2, _ref3,
       _this = this;
     done = this.async();
     if ((_ref = (_base = this.data.options).success) == null) {
@@ -15,9 +15,12 @@ module.exports = function(grunt) {
     if ((_ref2 = (_base2 = this.data.options).always) == null) {
       _base2.always = [];
     }
+    if ((_ref3 = (_base3 = this.data.options).nofail) == null) {
+      _base3.nofail = false;
+    }
     return runner.run(this.data, function(exitCode) {
       var success;
-      success = exitCode === 0;
+      success = (exitCode === 0) || _this.data.options.nofail;
       grunt.task.run(success ? _this.data.options.success : _this.data.options.failure);
       grunt.task.run(_this.data.options.always);
       return done(success);
